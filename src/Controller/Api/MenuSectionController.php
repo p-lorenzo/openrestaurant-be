7365c11f33b52b7029eb895a97e007a8d771e29f
p-lorenzo <lorenzo.pesce.1996@gmail.com>
2021-09-18 13:20:07 +0200

    Add MenuSection detail and update endpoints

--- src/Controller/Api/MenuSectionController.php
+++ src/Controller/Api/MenuSectionController.php
@@ -7,6 +7,7 @@ use App\Repository\MenuSectionRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -62,28 +63,24 @@ class MenuSectionController extends AbstractController
     /**
      * @Route("/update/{id}", name="app_api_menusection_update", methods={"POST"})
      */
-    // public function update(MenuSection $menuSection, Request $request): JsonResponse
-    // {
-    //     $data = json_decode($request->getContent(), true);
+    public function update(MenuSection $menuSection, Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
 
-    //     $name = $data['name'];
-    //     $description = $data['description'];
-    //     $price = $data['price'];
-    //     $quantity = $data['quantity'];
+        if (null == $data['title']) {
+            return $this->json(['message' => 'Expecting mandatory parameters!'], Response::HTTP_BAD_REQUEST);
+        }
 
-    //     if (empty($name) || empty($price) || empty($quantity)) {
-    //         throw new NotFoundHttpException('Expecting mandatory parameters!');
-    //     }
+        $title = $data['title'];
+        $sorting = $data['sorting'];
 
-    //     $menuSection->setName($name)
-    //         ->setPrice($price)
-    //         ->setQuantity($quantity)
-    //         ->setDescription($description);
-    //     $this->entityManager->persist($menuSection);
-    //     $this->entityManager->flush();
+        $menuSection->setTitle($title)
+            ->setSorting($sorting);
+        $this->entityManager->persist($menuSection);
+        $this->entityManager->flush();
 
-    //     return new JsonResponse(['status' => 'Sezione menu aggiornata!'], Response::HTTP_CREATED);
-    // }
+        return new JsonResponse(['status' => 'Sezione aggiornata!'], Response::HTTP_OK);
+    }
 
     /**
      * @Route("/delete/{id}", name="app_api_menusection_delete", methods={"DELETE"})
@@ -94,5 +91,13 @@ class MenuSectionController extends AbstractController
         $this->entityManager->flush();
 
         return new JsonResponse(['status' => 'Sezione menu rimossa!'], Response::HTTP_NO_CONTENT);
+    }
+
+    /**
+     * @Route("/{id}", name="app_api_menusection_detail", methods={"GET"})
+     */
+    public function detail(MenuSection $menuSection)
+    {
+        return $this->json($menuSection, Response::HTTP_OK, [], ['groups' => 'public']);
     }
 }
